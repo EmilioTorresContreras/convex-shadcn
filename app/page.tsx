a70e5f9cad50ab6584363020b964c3e2d6321fdb
root <root@Emilio.>
2025-04-24 13:26:44 -0600

    Se modificó el posicionamiento del botón de crear

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { TablaEstudiantes } from "../components/tabla-estudiantes";
 export default function Home() {
   return (
     <main className="container mx-auto py-10">
-      <h1 className="text-2xl font-bold mb-4">Lista de Estudiantes</h1>
       <TablaEstudiantes />
     </main>
   );
